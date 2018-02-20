a19cb77eb3ec1b6e16dd4cf4755cfa4f94e4c242
Dorian Lux <lux@student.tugraz.at>
2018-02-20 17:43:09 +0100

    ok giving up for today. WS seems to work but no idea how the protokoll is

--- servermonitor/testConversionServer.py
+++ servermonitor/testConversionServer.py
@@ -3,15 +3,20 @@ from scratchtocatrobat.tools.logger import setup_logging
 from scratchtocatrobat.tools.logger import log
 from scratchtocatrobat.tools.helpers import _setup_configuration
 import traceback
+import websocket
+from websocketserver.protocol.command.command import COMMAND_AUTHENTICATE
+from websocketserver.protocol.command.command import COMMAND_RETRIEVE_INFO
+from websocketserver.protocol.command.command import COMMAND_SCHEDULE_JOB
+
 configpath = "config/default.ini"
 
 def main():
     config = _setup_configuration(configpath)
     setup_logging()
-    webapirul = config.config_parser.get("Scratch2CatrobatConverter", "webapiurl")
+    webapirul = config.config_parser.get("Scratch2CatrobatConverter", "webapiurl") #http://scratch2.catrob.at/
     testWebApi(webapirul)
-    pass
-
+    conversionurl = config.config_parser.get("Scratch2CatrobatConverter", "conversionurl") #http://scratch2.catrob.at/convertersocket
+    testConversion(conversionurl)
 
 
 def testWebApi(url):
@@ -21,7 +26,7 @@ def testWebApi(url):
         conn.request("GET","/")
         r1 = conn.getresponse()
         status = r1.status
-        if(status == 200):
+        if status == 200:
             log.info("WebApi is up and running")
         else:
             log.error("WebApi Http status not OK, status is:" + str(status))
@@ -33,6 +38,15 @@ def testWebApi(url):
         except:
             pass
     return
+
+
+def testConversion(conversionurl):
+    ws = websocket.WebSocket()
+    ws.connect(conversionurl)
+    ws.send(COMMAND_AUTHENTICATE)
+    ws.send(COMMAND_SCHEDULE_JOB)
+    pass
+
 
 if __name__ == '__main__':
     main()
